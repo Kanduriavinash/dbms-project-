a77f026babff24bbf04439892cf632428b2f3164
Avinash K <kanduriavinashdj9666@gmail.com>
2026-06-16 09:35:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config:
     # PostgreSQL connection string format: postgresql://username:password@host:port/database
     # Default to PostgreSQL (can be overridden by DATABASE_URL environment variable)
